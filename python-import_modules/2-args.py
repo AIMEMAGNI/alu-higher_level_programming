1119178e17a64555caa9daf94fa7771e5a86bc05
Aime <a.ndayishim@alustudent.com>
2023-02-22 11:48:21 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# jbcclms# jbcclms# jbcclmis
-if __name__ == "__main__" :
+if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     if count == 0:
@@ -9,5 +8,5 @@ if __name__ == "__main__" :
         print("1 argument:")
     else :
         print("{} arguments:". format(count))
-    for i in range(count) :
+    for i in range(count):
         print("{}: {}". format(i + 1, sys.argv[i + 1]))
