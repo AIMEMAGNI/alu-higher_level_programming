988b3117b7321d4ab764ffc529471697ef23d7f8
Aime <a.ndayishim@alustudent.com>
2023-03-21 10:36:32 +0200

    done

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,20 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         """ A function that prints sorted list items"""
-
-        print(sorted(list))
-
-
-def main():
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
-
-
-main()
+        temp_list = self[:]
+        print(sorted(temp_list))
