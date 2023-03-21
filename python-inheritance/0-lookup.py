93cad89489554a8d0931b7fde9643834e7e85fa8
Aime <a.ndayishim@alustudent.com>
2023-03-21 09:48:21 +0200

    done

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ A program to list attributes and methods of an object"""
 def lookup(obj):
-
-
     """ A function that list attributes and methods of an object"""
     return dir(obj)
