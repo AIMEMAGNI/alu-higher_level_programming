53b2006380a710d2677a244ce9b35c1544f08019
Aime <a.ndayishim@alustudent.com>
2023-03-21 10:05:59 +0200

    done

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A class to list items """
+
+
+class MyList(list):
+    """ A class to list items """
+
+    def print_sorted(self):
+        """ A function that prints sorted list items"""
+
+        print(sorted(list))
