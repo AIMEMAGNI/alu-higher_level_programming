88d95b3a85b52c472242acaa8bd12e9301e88719
Aime <a.ndayishim@alustudent.com>
2023-03-23 08:50:14 +0200

    done

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Geometry class'''
+
+
+class BaseGeometry:
+    '''Geometry class'''
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if value.isdigit():
+            if value <= 0:
+                raise ValueError("<name> must be greater than 0")
+        else:
+            raise TypeError("<name> must be an integer")
