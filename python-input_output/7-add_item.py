b92645ffe70ac54fcecb01d46563d39ed3db11e5
Aime Magnifique NDAYISHIMIYE <82208013+AIMEMAGNI@users.noreply.github.com>
2023-03-24 09:33:57 +0100

    done

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# script that adds all arguments to a Python list,
-# and then save them to a file
 """
     script that adds all arguments to a Python list,
     and save them to a file
