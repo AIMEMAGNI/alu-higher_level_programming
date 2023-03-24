0948d4028c3d028379415c1c29bc16bbbdb38343
Ainme <a.ndayishim@alustudent.com>
2023-03-24 09:06:34 +0200

    done

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
