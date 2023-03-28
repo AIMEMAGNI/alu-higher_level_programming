e6d1668f774f23ef1a07c103c862869052c429a6
Aime <a.ndayishim@alustudent.com>
2023-03-28 22:06:22 +0200

    done

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
---  lists all records of the table, Don’t list rows without a name value
-SELECT `name`, `score` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
+-- lists all records of the table, Don’t list rows without a name value
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC;
